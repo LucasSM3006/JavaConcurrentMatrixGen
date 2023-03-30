1bbdc893f8fc702ecd678723a0b29213575d0f34
LucasSasso <88862803+LucasSM3006@users.noreply.github.com>
2023-03-30 17:43:19 -0300

    Simple code to run the matrix generator.

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int numRows = 4;
+        int numCols = 4;
+
+        float[][] matrix = new float[numRows][numCols];
+
+        MatrixGenerator generator = new MatrixGenerator(matrix);
+        generator.run();
+
+        // Print out the generated matrix
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
